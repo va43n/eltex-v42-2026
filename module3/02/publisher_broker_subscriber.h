f905fe7fbe488a177653d43a647ac57bf1ef56ba
va43n <va43nplay@gmail.com>
2026-08-04 12:48:35 +0700

    work on 02 and 03 is started

---
+++ module3/02/publisher_broker_subscriber.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define SUCCESS 111
+#define FAILURE -111
+
+//
+
---
+++ module3/03/p2p.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define SUCCESS 111
+#define FAILURE -111
+
+// int parse_input(int argc, char* argv[]);
+// int check_if_queue_is_created(char* queue_name);
+// int create_queue(char* queue_name);
+
+
+// int send_message(char* message);
+// int recv_message(char** message);
