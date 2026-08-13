e87e0caf2070b2c6a47309d6b64fada028d67fac
va43n <va43nplay@gmail.com>
2026-08-13 23:23:07 +0700

    work on 08 is started

---
+++ module3/08/main.c
@@ -0,0 +1,7 @@
+#include "raw_udp.h"
+
+int main() {
+  printf("test\n");
+
+  return 0;
+}
---
+++ module3/08/raw_udp.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define SUCCESS 111
+#define FAILURE -111
